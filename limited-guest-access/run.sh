9face15b08f63c53669ab847788940d2fdc32ae7
Dillon Nichols <dillon.nichols@innomotics.com>
2026-01-14 16:56:30 -0500

    do I need this check?

--- limited-guest-access/run.sh
+++ limited-guest-access/run.sh
@@ -11,24 +11,10 @@ if [ $is_ssl_active = true ]; then
     fi;
 fi;
 
-# Start nginx
-if ! pgrep nginx >/dev/null; then
-    nginx
+if ! pgrep "nginx" > /dev/null; then
+    nginx && php-fpm83
 fi
 
-# ---- PHP-FPM auto-detection (CRITICAL PART) ----
-for php_fpm in \
-    /usr/sbin/php-fpm83 \
-    /usr/sbin/php-fpm82 \
-    /usr/sbin/php-fpm8 \
-    /usr/sbin/php-fpm \
-    /usr/bin/php-fpm83 \
-    /usr/bin/php-fpm82 \
-    /usr/bin/php-fpm8 \
-    /usr/bin/php-fpm
-do
-    if [ -x "$php_fpm" ]; then
-        bashio::log.info "Using PHP-FPM binary: $php_fpm"
-        exec "$php_fpm" -F
-    fi
-done
+while true; do sleep 1000; done
+
+exec "$@"
